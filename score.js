ee05a063bbaf7430909fe09db8f161d8a908a976
Priyam7302 <priyamvada7302@gmail.com>
2025-08-14 15:14:51 +0530

    adding score and timer functionality

--- score.js
+++ score.js
@@ -26,7 +26,9 @@ const quiz = [
 
 let quesArr = [];
 let score = 0;
-let score_para = document.querySelector(".score");
+
+let score_head = document.querySelector(".score");
+
 function getRandomNumber() {
   let randomNum = Math.floor(Math.random() * quiz.length);
   if (quesArr.includes(randomNum)) {
@@ -38,55 +40,79 @@ function getRandomNumber() {
 }
 
 let container = document.querySelector(".container");
-let cnt = 0;
+let cnt = 1;
+let timeCnt = 5;
+let timer = document.querySelector(".timer");
+console.log(timer.innerText);
+
 fn();
+setTime();
 let timing = setInterval(() => {
   cnt++;
   fn();
-}, 2000);
+  setTime();
+}, 5000);
+
+function setTime() {
+  let timerFun = setInterval(() => {
+    timeCnt--;
+    if (timeCnt <= 0) {
+      clearInterval(timerFun);
+      timeCnt = 5;
+    }
+    timer.innerText = timeCnt;
+
+    console.log(timeCnt);
+  }, 1000);
+}
 
 function fn() {
-  let quesNum = getRandomNumber();
-  const parent = document.querySelector(".question");
-
-  console.log(quesNum);
-
-  const question = document.querySelector(".qn-para");
-  question.innerText = quiz[quesNum].question;
-
-  const answer = document.querySelector(".answer-para");
-
-  //   answer.innerText = quiz[quesNum].answer;
-
-  const imgQn = document.querySelector("img");
-  imgQn.src = "";
-  if (
-    quiz[quesNum].imgUrl &&
-    (quiz[quesNum].imgUrl.toLowerCase().endsWith(".jpg") ||
-      quiz[quesNum].imgUrl.toLowerCase().endsWith(".png") ||
-      quiz[quesNum].imgUrl.toLowerCase().endsWith(".gif"))
-  ) {
-    imgQn.src = quiz[quesNum].imgUrl;
-  }
-
-  const optionDiv = document.querySelector("div");
-  const options = document.querySelectorAll(".option");
-
-  for (let j = 0; j < 4; j++) {
-    options[j].innerText = quiz[quesNum].options[j];
-  }
-
-  for (let i = 0; i < options.length; i++)
-    options[i].addEventListener("click", checkAns);
-
-  function checkAns(e) {
-    if (e.target.innerText === answer) {
-      score += 1;
-      score_para.innerText = score;
-    }
-  }
-
-  if (cnt == 3) {
+  if (cnt > quiz.length) {
     clearInterval(timing);
+    score_head.innerText = score;
+    console.log("Your score is: " + score);
+  } else {
+    let quesNum = getRandomNumber();
+    const parent = document.querySelector(".question");
+
+    const question = document.querySelector(".qn-para");
+    question.innerText = quiz[quesNum].question;
+
+    const imgQn = document.querySelector("img");
+    imgQn.src = "";
+    if (
+      quiz[quesNum].imgUrl &&
+      (quiz[quesNum].imgUrl.toLowerCase().endsWith(".jpg") ||
+        quiz[quesNum].imgUrl.toLowerCase().endsWith(".png") ||
+        quiz[quesNum].imgUrl.toLowerCase().endsWith(".gif"))
+    ) {
+      imgQn.src = quiz[quesNum].imgUrl;
+    }
+
+    const optionDiv = document.querySelector("div");
+    const options = document.querySelectorAll(".option");
+    for (let j = 0; j < 4; j++) {
+      options[j].innerText = quiz[quesNum].options[j];
+    }
+
+    let optionClicked = false;
+    for (let i = 0; i < options.length; i++) {
+      options[i].style.color = "black";
+
+      options[i].addEventListener("click", clickFn);
+
+      function clickFn(e) {
+        let userans = e.target.innerText;
+
+        if (optionClicked == false) {
+          optionClicked = true;
+          e.target.style.color = "red";
+        }
+
+        if (quiz[quesNum].answer === userans) {
+          score++;
+        }
+      }
+    }
   }
 }
